2ee079c150f8d0abdb2479b6676d4e10ed9d90c3
Jade-jaeho.shin <jaeho.shin@lulumedic.com>
2025-09-22 10:36:41 +0900

    feat : SST/TTS page ~ 145

--- src/main/java/com/example/springai/ch5/controller/AiControllerAudio.java
+++ src/main/java/com/example/springai/ch5/controller/AiControllerAudio.java
@@ -1,16 +1,20 @@
 package com.example.springai.ch5.controller;
 
 import com.example.springai.ch5.service.AiServiceAudio;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.content.MediaContent;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @RestController
@@ -51,5 +55,35 @@ public class AiControllerAudio {
         log.info("START CHAT TEXT...");
         Map<String,String> response = aiService.chatText(question);
         return response;
+    }
+
+    @PostMapping(
+            value = "/chat-voice-stt-llm-tts",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+    )
+    public void chatVoiceSstLlmTts(@RequestParam("question") MultipartFile question,
+                                   HttpServletResponse response) throws IOException {
+        log.info("START CHAT VOICE SST LLM TTS...");
+        Flux<byte[]> flux = aiService.chatVoiceSttLlmTts(question);
+
+        OutputStream outputStream = response.getOutputStream();
+
+        for(byte[] chunk : flux.toIterable()) {
+            outputStream.write(chunk);
+            outputStream.flush();
+        }
+    }
+
+    @PostMapping(
+            value = "/chat-voice-one-model",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
+    )
+    public byte[] chatVoiceOneModel(@RequestParam("question") MultipartFile question, HttpServletResponse response) throws IOException {
+        log.info("START CHAT VOICE ONE MODEL...");
+        byte[] bytes = aiService.chatVoiceOneModel(question, question.getContentType());
+
+        return bytes;
     }
 }
--- src/main/java/com/example/springai/ch5/service/AiServiceAudio.java
+++ src/main/java/com/example/springai/ch5/service/AiServiceAudio.java
@@ -1,25 +1,33 @@
 package com.example.springai.ch5.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.openai.OpenAiAudioSpeechModel;
-import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
-import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
-import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.content.Media;
+import org.springframework.ai.openai.*;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.ai.openai.audio.speech.SpeechPrompt;
 import org.springframework.ai.openai.audio.speech.SpeechResponse;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeType;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class AiServiceAudio {
 
@@ -93,6 +101,84 @@ public class AiServiceAudio {
         response.put("audio", base64Audio);
 
         return response;
+    }
+
+    public Flux<byte[]> ttsFlux(String text) {
+        // 모델 옵션 설정
+        OpenAiAudioSpeechOptions options = OpenAiAudioSpeechOptions.builder()
+                .model("gpt-4o-mini-tts")
+                .voice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY)
+                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .speed(1.0f)
+                .build();
+
+        // 프롬프트 생성
+        SpeechPrompt prompt = new SpeechPrompt(text, options);
+
+        // 모델 요청 응답 생성
+        Flux<SpeechResponse> reponse = openAiAudioSpeechModel.stream(prompt);
+        Flux<byte[]> flux = reponse.map(speechResponse -> speechResponse.getResult().getOutput());
+
+        return flux;
+    }
+
+    // 음성으로 질문/대답 하기
+    public Flux<byte[]> chatVoiceSttLlmTts(MultipartFile file) throws IOException {
+        String textQuestion = stt(file);
+        String textAnswer = chatClient.prompt()
+                .system("50자 이내로 답변 해 주세요")
+                .user(textQuestion)
+                .options(ChatOptions.builder()
+                        .maxTokens(100)
+                        .build())
+                .call()
+                .content();
+
+        // tts를 이용해서 비동기 음성 데이터 얻기
+        Flux<byte[]> flux = ttsFlux(textAnswer);
+        return flux;
+    }
+
+    public byte[] chatVoiceOneModel(MultipartFile file, String mimeType) throws IOException {
+        // 음성 데이터를 resource로 생성
+        Resource resource = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        };
+
+        // 사용자 메시지 생성
+        UserMessage userMessage = UserMessage.builder()
+                .text("제공되는 음성에 맞는 자연스러운 대화로 이어주세요.")
+                .media(new Media(MimeType.valueOf(mimeType), resource))
+                .build();
+
+        // 모델 옵션 설정
+        ChatOptions chatOptions = OpenAiChatOptions.builder()
+                .model(OpenAiApi.ChatModel.GPT_4_O_AUDIO_PREVIEW)
+                .outputModalities(List.of("text", "audio"))
+                .outputAudio(new OpenAiApi.ChatCompletionRequest.AudioParameters(
+                        OpenAiApi.ChatCompletionRequest.AudioParameters.Voice.ALLOY,
+                        OpenAiApi.ChatCompletionRequest.AudioParameters.AudioResponseFormat.MP3
+                ))
+                .build();
+
+        ChatResponse response = chatClient.prompt()
+                .system("50자 이내로 답변해 주세요")
+                .messages(userMessage)
+                .options(chatOptions)
+                .call()
+                .chatResponse();
+
+        AssistantMessage assistantMessage = response.getResult().getOutput();
+
+        String textAnswer = assistantMessage.getText();
+        log.info("텍스트 응답 : {}", textAnswer);
+
+        byte[] audioAnswer = assistantMessage.getMedia().get(0).getDataAsByteArray();
+        return audioAnswer;
+
     }
 
 }
