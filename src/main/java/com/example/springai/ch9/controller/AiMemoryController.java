77e14f1d8017ecdc2e8611453cefeced5be38021
sshinn0224 <sshinn0224@gmail.com>
2025-09-26 16:57:18 +0900

    page ~274 : chat memory advisor with vector

---
+++ src/main/java/com/example/springai/ch9/controller/AiMemoryController.java
@@ -0,0 +1,29 @@
+package com.example.springai.ch9.controller;
+
+import com.example.springai.ch9.service.AiMemoryService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai")
+@Slf4j
+@RequiredArgsConstructor
+public class AiMemoryController {
+
+    private final AiMemoryService aiMemoryService;
+
+    @PostMapping(
+            value = "/chat",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public String doRequest(@RequestParam("question") String question, HttpSession session) {
+        return aiMemoryService.chat(question, session.getId());
+    }
+}
