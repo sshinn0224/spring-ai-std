dd0483180531a5db1f3c9a9e89dec250f059aa03
sshinn0224 <sshinn0224@gmail.com>
2025-09-27 00:12:23 +0900

    page ~286 : chat memory advisor by RDBMS

--- src/main/java/com/example/springai/ch9/controller/AiMemoryController.java
+++ src/main/java/com/example/springai/ch9/controller/AiMemoryController.java
@@ -1,6 +1,8 @@
 package com.example.springai.ch9.controller;
 
+import com.example.springai.ch9.service.AiMemoryRDBService;
 import com.example.springai.ch9.service.AiMemoryService;
+import com.example.springai.ch9.service.AiMemoryVectorService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,13 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AiMemoryController {
 
 //    private final AiMemoryService aiMemoryService;
+//    private final AiMemoryVectorService aiMemoryVectorService;
+    private final AiMemoryRDBService aiMemoryRDBService;
 
-//    @PostMapping(
-//            value = "/chat",
-//            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-//            produces = MediaType.TEXT_PLAIN_VALUE
-//    )
-//    public String doRequest(@RequestParam("question") String question, HttpSession session) {
-//        return aiMemoryService.chat(question, session.getId());
-//    }
+    @PostMapping(
+            value = "/chat",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    public String doRequest(@RequestParam("question") String question, HttpSession session) {
+        return aiMemoryRDBService.chat(question, session.getId());
+    }
 }
